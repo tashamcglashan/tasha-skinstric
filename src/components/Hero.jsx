2bed31737118c50fbd421568bda018075e225bd5
Tasha McGlashan <tmcglashan77@gmail.com>
2025-07-10 15:49:46 -0700

    created Hero, added css to App

---
+++ src/components/Hero.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './Hero.css';
+import { FaPlay } from 'react-icons/fa';
+
+export default function Hero() {
+  const [hoverDirection, setHoverDirection] = useState(null);
+
+  return (
+    <section className={`hero ${hoverDirection}`}>
+      <div className="hero__top">
+        {/* Left Section */}
+        <div
+          className="hero__left"
+          onMouseEnter={() => setHoverDirection('hover-left')}
+          onMouseLeave={() => setHoverDirection(null)}
+        >
+          <div className="hover-container">
+          <div className="diamond">
+            <div className="arrow-wrapper">
+              <FaPlay className="arrow left" />
+            </div>
+          </div>
+          <p className="hero__label">Discover A.I.</p>
+        </div>
+        </div>
+
+        {/* Center Section */}
+        <div className="hero__center">
+          <h1 className="hero__title">
+            Sophisticated<br />skincare
+          </h1>
+        </div>
+
+        {/* Right Section */}
+        <div
+          className="hero__right"
+          onMouseEnter={() => setHoverDirection('hover-right')}
+          onMouseLeave={() => setHoverDirection(null)}
+        >
+            <div className="hover-container">
+          <p className="hero__label">Take Test</p>
+          <div className="diamond">
+            <div className="arrow-wrapper">
+              <FaPlay className="arrow right" />
+            </div>
+          </div>
+        </div>
+      </div>
+        </div>
+
+      {/* Bottom Paragraph */}
+      <div className="hero__bottom">
+        <p>
+          SKINSTRIC DEVELOPED AN A.I. THAT CREATES A HIGHLY-PERSONALIZED ROUTINE
+          TAILORED TO WHAT YOUR SKIN NEEDS.
+        </p>
+      </div>
+    </section>
+  );
+}
